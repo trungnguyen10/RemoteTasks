d652ed15d8e0042bf513b4c95850b76122742577
Trung Nguyen <trungnguyen2809@gmail.com>
2021-12-02 19:08:40 -0600

    Update TaskManager Class * adding the fields

--- app/src/main/java/com/remotetasks/TaskManager.java
+++ app/src/main/java/com/remotetasks/TaskManager.java
@@ -1,4 +1,17 @@
 package com.remotetasks;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class TaskManager {
+    private Parent mParent;
+    private ArrayList<Child> mListOfChild;
+    private Task mTask;
+    private IReward mReward;
+    private LocalDateTime mStartTime;
+    private LocalDateTime mEndTime;
+    private Duration mExtensionTime;
+
+
 }
