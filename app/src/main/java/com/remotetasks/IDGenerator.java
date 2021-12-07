08e6d751bca771481e6d7c037e63055082238229
Trung Nguyen <trungnguyen2809@gmail.com>
2021-12-07 09:14:11 -0600

    Fix bug in IDGenerator

--- app/src/main/java/com/remotetasks/IDGenerator.java
+++ app/src/main/java/com/remotetasks/IDGenerator.java
@@ -4,11 +4,11 @@ public class IDGenerator {
     private static int userID = 0;
     private static int taskID = 0;
 
-    public final int generateUserID() {
+    public static final int generateUserID() {
         return userID++;
     }
 
-    public final int generateTaskID() {
+    public static final int generateTaskID() {
         return taskID++;
     }
 }
